2547579c058c99253b6cc8cab4dff28d9711c7b9
João Gonçalves <joaofgo@gmail.com>
2017-11-24 01:46:08 +0000

    Adds P4 and new data sample data

--- P3_rasterPackage_solutions_I-v1.R
+++ P3_rasterPackage_solutions_I-v1.R
@@ -5,6 +5,8 @@ Below are the solutions to <a href="http://r-exercises.com/2017/12/27/exercises-
 
 
 <!--begin.rcode, echo=TRUE, eval=TRUE, message=FALSE
+
+
 ####################
 #                  #
 #    Exercise 1    #
@@ -13,29 +15,41 @@ Below are the solutions to <a href="http://r-exercises.com/2017/12/27/exercises-
 
 library(raster)
 
-## Create a folder named data-raw inside the working directory to place downloaded data
+## Create a folder named data-raw inside the working directory to place downloaded data     
+
 if(!dir.exists("./data-raw")) dir.create("./data-raw")
 
-## If you run into download problems try changing: method = "wget"
+## If you run into download problems try changing: method = "wget"        
+
 download.file("https://raw.githubusercontent.com/joaofgoncalves/R_exercises_raster_tutorial/master/data/srtm_pnpg.zip", 
               "./data-raw/srtm_pnpg.zip", method = "auto")
 
-## Uncompress the zip file
+## Uncompress the zip file        
+
 unzip("./data-raw/srtm_pnpg.zip", exdir = "./data-raw")
 
 rst <- raster("./data-raw/srtm_pnpg.tif")
 
-# Number of rows, columns and layers
+# Number of rows, columns and layers        
+
 dim(rst)
 
 # Number of rows
+
 nrow(rst)
+
 # Number of columns
+
 ncol(rst)
+
 # Number of layers
+
 nlayers(rst)
+
 # Number of cells
+
 ncell(rst)
+
 
 ####################
 #                  #
@@ -44,10 +58,13 @@ ncell(rst)
 ####################
 
 # Spatial resolution (pixel size in x and y dimensions)
+
 res(rst)
 
 # Coordinate reference system: Datum/Ellipsoid: WGS 1984, Projection: UTM 29N, Units: meters
+
 crs(rst)
+
 
 ####################
 #                  #
@@ -56,13 +73,17 @@ crs(rst)
 ####################
 
 # The extent object
+
 extent(rst)
 
 # Lenght in meters
+
 xmax(rst) - xmin(rst)
 
 # Height in meters
+
 ymax(rst) - ymin(rst)
+
 
 ####################
 #                  #
@@ -71,9 +92,11 @@ ymax(rst) - ymin(rst)
 ####################
 
 # Mean
+
 cellStats(rst, mean)
 
 # Standard-deviation
+
 cellStats(rst, sd)
 
 ####################
@@ -83,6 +106,7 @@ cellStats(rst, sd)
 ####################
 
 # Raster quantiles
+
 cellStats(rst, stat = function(x, ...) quantile(x, probs = c(0.01, 0.25, 0.5, 0.75, 0.99), ...))
 
 ####################
@@ -92,9 +116,11 @@ cellStats(rst, stat = function(x, ...) quantile(x, probs = c(0.01, 0.25, 0.5, 0.
 ####################
 
 # Get values for the raster (memory may be an issue)
+
 x <- values(rst)
 
 # Make the plot (notice higher deviations for values above the average)
+
 qqnorm(x)
 qqline(x)
 
@@ -107,15 +133,19 @@ qqline(x)
 set.seed(12345)
 
 # Generate random points with uniform distribution bounded by the raster extent
+
 xyRandPoints <- data.frame(x = runif(100, xmin(rst), xmax(rst)), y = runif(100, ymin(rst), ymax(rst)))
 
 # Convert the initial data frame into a SpatialPoints objects for clarity
+
 xyRandPoints <- SpatialPoints(xyRandPoints, proj4string = crs(rst))
 
 # Extract values
+
 xyVals <- extract(rst, xyRandPoints)
 
 print(xyVals)
+
 
 ####################
 #                  #
@@ -129,6 +159,7 @@ rstStack <- stack(rst, rstFt)
 
 print(rstStack)
 
+
 ####################
 #                  #
 #    Exercise 9    #
@@ -136,18 +167,22 @@ print(rstStack)
 ####################
 
 # Bounding coordinates
+
 xmin <- 554615
 xmax <- 589015 
 ymin <- 4618355 
 ymax <- 4654705
 
 # Create the extent object by defining the bounding coordinates
+
 newExtent <- extent(xmin, xmax, ymin, ymax)
 
 # Crop
+
 cropRst <- crop(rst, newExtent)
 
 print(cropRst)
+
 
 ####################
 #                  #
@@ -156,11 +191,15 @@ print(cropRst)
 ####################
 
 # Target CRS
+
 targetCRS <- CRS("+init=epsg:3035")
 
 # Reproject data
+
 rst_ETRS89_LAEA <- projectRaster(rst, crs = targetCRS, res = 100, method = 'bilinear')
 
 print(rst_ETRS89_LAEA)
+
+
 
 end.rcode-->
